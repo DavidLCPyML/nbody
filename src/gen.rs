cd87fac91ff6e3eff3e9dc38199ed6b2aa8e13f6
DavidLCPyML <email2david2015@gmail.com>
2023-06-11 15:38:13 -0500

    galaxy creation, actual rendering NYI

--- src/gen.rs
+++ src/gen.rs
@@ -2,19 +2,21 @@ use cgmath::{
     prelude::*,
     {Point3, Vector3},
 };
+use std::f32::consts::PI;
+use rand::prelude::*;
 
 const G: f64 = 6.67408E-11;
 const ARMS: u32 = 4;
 
 pub struct Particle {
-    pos: Point3<f32>,
-    velocity: Vector3<f32>,
-    mass: f64,
-    density: f64,
+  pos: Point3<f64>,
+  velocity: Vector3<f64>,
+  pub mass: f64,
+  pub density: f64,
 }
 
 impl Particle {
-    fn new(pos: Point3<f32>, velocity: Vector3<f32>, mass: f64, density: f64) -> Self {
+    pub fn new(pos: Point3<f64>, velocity: Vector3<f64>, mass: f64, density: f64) -> Self {
         Self {
             pos,
             velocity,
@@ -24,26 +26,93 @@ impl Particle {
     }
 }
 
+pub enum Galaxy {
+  Particle {
+    pos: Point3<f64>,
+    velocity: Vector3<f64>,
+    mass: f64,
+    density: f64,
+  },
+  Structures {
+    num_particles: u32,
+    center_pos: Point3<f64>,
+    center_mass: f64,
+    center_velocity: Vector3<f64>,
+    center_density: f64,
+    normal: Vector3<f64>,
+  }
+}
+
+pub struct Galaxies {
+  pub galaxy: Vec<Galaxy>,
+}
+
+impl Galaxies {
+  pub fn new(&self) -> Vec<Particle> {
+    let mut particles = Vec::new();
+    for i in &self.galaxy {
+      particles.push(match i {
+        Galaxy::Particle {
+          pos,
+          velocity,
+          mass,
+          density,
+        } => Particle::new(*pos, *velocity, *mass, *density),
+        Galaxy::Structures {
+          num_particles,
+          center_pos,
+          center_mass,
+          center_velocity,
+          center_density,
+          normal,
+        } => Particle::new(*center_pos, *center_velocity, *center_mass, *center_density),
+      })
+    }
+    for i in &self.galaxy {
+      if let Galaxy::Structures {
+        num_particles,
+        center_pos,
+        center_mass,
+        center_velocity,
+        center_density,
+        normal,
+      } = i
+      {
+        create_galaxy(
+          &mut particles,
+          *num_particles,
+          *center_pos,
+          *center_mass,
+          *center_velocity,
+          *center_density,
+          *normal,
+        );
+      }
+    }
+    particles
+  }
+}
+
 pub fn create_galaxy(
     particles: &mut Vec<Particle>,
     num_particles: u32,
-    center_pos: Point3<f32>,
+    center_pos: Point3<f64>,
     center_mass: f64,
-    center_velocity: Vector3<f32>,
+    center_velocity: Vector3<f64>,
     center_density: f64,
-    mass: f64,
-    density: f64,
-    mut normal: Vector3<f32>,
+    mut normal: Vector3<f64>,
 ) {
   normal = normal.normalize();
-  let tangent: Vector3<f32> = normal.cross(Vector3::new(normal.y, -normal.x, normal.z));
+  let tangent: Vector3<f64> = normal.cross(Vector3::new(normal.y, -normal.x, normal.z));
   let bitangent = normal.cross(tangent);
   let radius: f64 = 3E12 as f64;
+  let mass = 1e30;
+  let density = 1e20;
 
   for _ in 0..num_particles / 5 {
-    let theta: f32 = thread_rng().gen::<f32>() * 2.0 * PI;
-    let dir: f32 = tangent * cos(theta) + bitangent * sin(theta);
-    let pos = center_pos + dir * radius * cos(theta);
+    let theta: f64 = thread_rng().gen::<f64>() * 2.0 * PI as f64;
+    let dir: Vector3<f64> = tangent * theta.cos() + bitangent * theta.cos();
+    let pos = center_pos + dir * radius * theta.cos();
     let speed = (G * center_mass / radius).sqrt();
     let fly_dir = dir.cross(normal); // check if necessary
     let velocity = center_velocity + fly_dir * speed;
@@ -51,11 +120,11 @@ pub fn create_galaxy(
   }
 
   for _ in 0..num_particles / 5 * 4 {
-    let arm = thread_rng().gen_range::<u32>(0, ARMS);
+    let arm: u32 = thread_rng().gen_range(0..ARMS);
 
-    let theta: f32 = arm / ARMS * 2.0 * PI;
-    let dir: f32 = tangent * cos(theta) + bitangent * sin(theta);
-    let pos = center_pos + dir * radius * cos(theta);
+    let theta: f64 = (arm / ARMS) as f64 * 2.0 * PI as f64;
+    let dir: Vector3<f64> = tangent * theta.cos() + bitangent * theta.sin();
+    let pos = center_pos + dir * radius * theta.cos();
     let speed = (G * center_mass / radius).sqrt();
     let fly_dir = dir.cross(normal); // check if necessary
     let velocity = center_velocity + fly_dir * speed;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
 use nbody::run;
+mod gen;
+
+use cgmath::{
+    Point3,
+    Vector3
+};
+
+fn defaults() -> gen::Galaxies {
+    gen::Galaxies {
+    galaxy: vec![
+        gen::Galaxy::Structures {
+            center_pos: Point3::new(1e9, 1e9, 0.0),
+            center_velocity: Vector3::new(1e5, 0.0, 0.0),
+            center_mass: 1e30,
+            num_particles: 10000,
+            normal: Vector3::new(1.0, 0.0, 0.0),
+            center_density: 1e20,
+        },
+        gen::Galaxy::Structures {
+            center_pos: Point3::new(-1e9, -1e9, 0.0),
+            center_velocity: Vector3::new(0.0, 0.0, 0.0),
+            center_mass: 1e30,
+            num_particles: 10000,
+            normal: Vector3::new(1.0, 1.0, 0.0),
+            center_density: 1e20,
+
+        },
+    ],
+}
+}
 
 fn main() {
+    let default: gen::Galaxies = defaults();
+    let particles = default.new();
+
+    // pollster::block_on(run(particles));
     pollster::block_on(run());
 }
 
